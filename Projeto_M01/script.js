e52acd6e3de53ddb8deaf8446d7ce03a96f1b53f
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-04-04 10:04:59 -0300

    cria primeiros arquivos do projeto
    Criando os arquivos principais da página já com uma pré-configuração de como vai ser a página e alguns campos para serem capturados no arquivo JavaScript.

---
+++ Projeto_M01/script.js
@@ -0,0 +1,13 @@
+//campos de input txt
+const nomeProduto = document.getElementById('nome-produto');
+const liProdutos = document.getElementById('lista-produtos');
+
+//campos de input number
+const qtdProduto = document.getElementById('qtd-produto');
+const valorProdutos = document.getElementById('valor-produtos');
+
+//buttons
+const btnAdcProd = document.getElementById('btn-adiciona');
+const btnAdcValor = document.getElementById('btn-valor');
+const btnDeletarLista = document.getElementById('deletar-itens');
+const btnDeletarItens = document.getElementById('deletar-lista');
