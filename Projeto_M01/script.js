ae8a51eecc135f17425fa92474c59c7215206890
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-04-05 12:17:26 -0300

    adiciona checkbox na table
    criado elemento dentro da função que cria a tabela, adicionando um checkbox em cada item adicionado.

--- Projeto_M01/script.js
+++ Projeto_M01/script.js
@@ -11,6 +11,7 @@ const btnAddProd = document.getElementById('btn-adiciona');
 const btnAdcValor = document.getElementById('btn-valor');
 const btnDeletarLista = document.getElementById('deletar-lista');
 const btnDeletarItens = document.getElementById('deletar-itens');
+const btnComprar = document.getElementById('btn-comprar')
 
 // Array dos produtos
 var lista = [];
@@ -18,7 +19,7 @@ var lista = [];
 // funcao de adicionar item
 function addItem() {
     // testa se existe algo escrito no campo  
-    if (nomeProduto.value) {
+    if (nomeProduto.value && qtdProduto.value && valorProdutos.value) {
         lista.push({            //insere um objeto com id/nome/qtde/valor no array
         id: Date.now(),  
         name: nomeProduto.value,
@@ -33,44 +34,31 @@ function addItem() {
       saveStorage();
       // chama função tabela
       listaTabela();
-    } else {
-      // aviso para campo vazio
-     // alert('Insira o nome de um item!');
+    // } else {
+    //     if(nomeProduto == ''){
+    //         alert('Insira o nome de um item!');
+    //     }if(qtdProduto == ''){
+    //         alert('Insira a quantidade de itens!');
+    //     }
+    // }
     }
 }
-
 // busca lista no localStorage
 var listaJSON = localStorage.getItem('lista');
 
-// verifica se tem algo do storage
-if (listaJSON) {
-    // atualiza lista oficial e tela
-    lista = JSON.parse(listaJSON);
-    addItem();
-    listaTabela();
-  }
+    // verifica se tem algo do storage
+    if (listaJSON) {
+        // atualiza lista oficial e tela
+        lista = JSON.parse(listaJSON);
+        addItem();
+        listaTabela();
+    }
 // funcao de salvar no localStorage
 function saveStorage() {
     var listaJSON = JSON.stringify(lista);
     // salva a lista
     localStorage.setItem('lista', listaJSON);
 }
-
-
-//  function deletarProduto(){
-//     let novaLista = [];
-//     lista.forEach(function(lista) {
-//         if(lista.id !== id) {
-//             novaLista.push(lista);
-//         }
-//     })
-//     console.log(novaLista)
-//     lista = novaLista;
-//     //addItem();
-// }
-
-
-
 
     //monta os itens na tabela
 function listaTabela(){
@@ -80,23 +68,28 @@ function listaTabela(){
     tabela.innerText = '';
 
     for(let i = 0; i < lista.length; i++) {
-
+       
         let cell = tabela.insertRow();
-
+        
         let tabela_check = cell.insertCell();
         let tabela_produto = cell.insertCell();
         let tabela_quantidade = cell.insertCell();
         let tabela_valor = cell.insertCell();
         let tabela_acao = cell.insertCell();
 
-        tabela_check.innerHtml = lista.id;
+        tabela_check.innerHtml = checkbox;
         tabela_produto.innerText = lista[i].name;
         tabela_quantidade.innerText = lista[i].quantidade;
         tabela_valor.innerText = lista[i].valor;
 
         //cria o checkbox dentro da table
-        let checkbox = document.createElement('checkbox');
-        checkbox.addEventListener('onclick', checkbox)
+        var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        // checkbox.name = "name";
+        // checkbox.value = "value";
+        // checkbox.id = "id";
+        console.log(checkbox.id)
+        tabela_check.appendChild(checkbox)
 
         //cria o elemento deletar clicando em uma imagem definida
         let imgDelete = document.createElement('img');
@@ -105,27 +98,17 @@ function listaTabela(){
         tabela_acao.appendChild(imgDelete);
 
         //addo evento de excluir o produto pelo id
-        //imgDelete.addEventListener("click", deletarProduto(lista.id));
-        
+        imgDelete.onclick = function (id){
+            var novaLista = [];
+            lista.forEach(function (lista){
+            if (lista.id !== id){
+                novaLista.push('lista');
+                }
+            })
+        lista = novaLista;
+        }; 
     }
 }
-
-
-// function deletarProduto(id) {
-        
-//     if(confirm('Deseja realmente deletar o id: ' + id)){
-
-//     let tabela = document.getElementById('tabela');
-
-//     for(let i = 0; i < lista.length; i++){
-
-//         if(lista[i].id == id){
-//             lista.splice(i, 1);
-//             tabela.deleteRow(i);
-//             }
-//         }
-//     }
-// }
 
 // objetivo de limpar a lista do local storage
 function deletaLista(){
@@ -141,9 +124,14 @@ btnDeletarLista.addEventListener('click', deletaLista)
 console.log(lista)
 
 
-
-// input[type="checkbox"]:checked ~ .lineThrough {
-//   color: #ccc;
-//   text-decoration: line-through;
-// }
-// coloquei a classe lineThrough no checkbox criado
+//addo evento de excluir o produto pelo id
+function removeProduto (id) {
+    var novaLista = [];
+    lista.forEach(function (lista){
+        if (lista.id !== id){
+            novaLista.push(lista);
+        }
+    })
+    lista = novaLista;
+    listaTabela();
+}; 
